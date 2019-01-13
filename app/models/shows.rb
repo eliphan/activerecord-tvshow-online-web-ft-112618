14d602a76ff277a6ef9a8ecdfb19faa2c398e9ca
Elise Phan <aliceconnic@gmail.com>
2019-01-13 01:01:38 +0000

    Automatically backed up by Learn

--- app/models/shows.rb
+++ app/models/shows.rb
@@ -10,4 +10,7 @@ class Show < ActiveRecord::Base
   def self.lowest_rating
     Show.minimum(:rating)
   end
+  
+  def self.least_popular_show
+    Show.where("rating = ?", self.lowest_rating).first
 end
